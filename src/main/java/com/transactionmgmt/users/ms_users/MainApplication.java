95e7ff8e6ff63de45e04b7efe7fe737ff1586124
LinaRamirez <lina.ramirez@sofka.com.co>
2025-08-23 14:48:24 -0500

    Create project

---
+++ src/main/java/com/transactionmgmt/users/ms_users/MainApplication.java
@@ -0,0 +1,13 @@
+package com.transactionmgmt.users.ms_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
