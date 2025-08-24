3aa1f90ecbf5ffb1f5c3cf463fff38351154a924
LinaRamirez <lina.ramirez@sofka.com.co>
2025-08-24 14:59:54 -0500

    Remove unused UpdateClientDto mapping from ClientDtoMapper

--- src/main/java/com/transactionmgmt/users/ms_users/service/mappers/ClientDtoMapper.java
+++ src/main/java/com/transactionmgmt/users/ms_users/service/mappers/ClientDtoMapper.java
@@ -4,7 +4,6 @@ package com.transactionmgmt.users.ms_users.service.mappers;
 import com.transactionmgmt.users.ms_users.domain.client.Client;
 import com.transactionmgmt.users.ms_users.service.dto.ClientDto;
 import com.transactionmgmt.users.ms_users.service.dto.CreateClientDto;
-import com.transactionmgmt.users.ms_users.service.dto.UpdateClientDto;
 import org.mapstruct.Mapper;
 
 import java.util.List;
@@ -15,6 +14,5 @@ public interface ClientDtoMapper {
     ClientDto toResponse (Client client);
     List<ClientDto> toResponseList(List<Client> clientList);
     Client toModel(CreateClientDto dto);
-    Client toModel (UpdateClientDto dto);
     
 }
